da40f6e41784b50dfb5e4047658b3f779c904e99
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-03-20 20:56:38 +0530

    build issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,4 @@
-export function cn(...classes: (string | undefined)[]) {
-  return classes.filter(Boolean).join(" ")
+export function cn(...classes: (string | undefined | false | null)[]) {
+  return classes.filter(Boolean).join(" ");
 }
-
 
