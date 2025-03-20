9c4aecc6669e4b22f718052b62524138714e7f5e
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-03-20 20:41:43 +0530

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,11 @@ export const metadata: Metadata = {
   title: 'Personal Portfolio - Aditya Pradhan',
   description: 'personal portfolio',
   icons: {
-    icon: '/favicon.ico'
+    icon: [
+       { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ]
   }
 }
 
