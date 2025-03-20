cd2fe425b6e1e055dc47f006e157120369e32218
AdiInfiniteLoop <adiyouaresosmart@gmail.com>
2025-03-20 20:50:48 +0530

    build issues

--- components/badge.tsx
+++ components/badge.tsx
@@ -4,7 +4,7 @@ import { cn } from "../lib/utils"
 interface BadgeProps {
   children: React.ReactNode
   variant?: "default" | "secondary" | "outline"
-  className?: string
+  className?: string | boolean
 }
 
 export function Badge({ children, variant = "default", className }: BadgeProps) {
